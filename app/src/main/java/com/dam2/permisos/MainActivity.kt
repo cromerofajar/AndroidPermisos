9544d433cc815e7b56a9f4c3817fd1c0e29c3ea5
cromerofajar <cromerofajar@danielcastelao.org>
2019-12-02 09:30:31 +0100

    Codigo permisos

---
+++ app/src/main/java/com/dam2/permisos/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.dam2.permisos
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.makeCall
+import org.jetbrains.anko.longToast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        permiso.setOnClickListener{camera(it)}
+        permiso2.setOnClickListener{llamar(it)}
+
+
+    }
+    private fun llamar(y: View?) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.CALL_PHONE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                longToast("permitido")
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    2
+                )
+            } else {
+                longToast("Rechazado permanentemente")
+
+            }
+        } else {
+            makeCall("112")
+        }
+    }
+
+    private fun camera(y: View?){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                longToast("Permitido")
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    1)
+            } else {
+                longToast("Rechazado permanentemente")
+
+            }
+        } else {
+            longToast("Ya permitido")
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent,0)
+        }
+    }
+}
