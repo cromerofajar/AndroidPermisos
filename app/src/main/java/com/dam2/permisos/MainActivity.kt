c5126ae1df97f3add22b4788c5d304939e14ceff
cromerofajar <cromerofajar@danielcastelao.org>
2019-12-02 10:56:09 +0100

    Añadir la imagen al programa

--- app/src/main/java/com/dam2/permisos/MainActivity.kt
+++ app/src/main/java/com/dam2/permisos/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.dam2.permisos
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import androidx.core.app.ActivityCompat
@@ -20,11 +24,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        permiso.setOnClickListener{camera(it)}
-        permiso2.setOnClickListener{llamar(it)}
+        permiso.setOnClickListener { camera(it) }
+        permiso2.setOnClickListener { llamar(it) }
 
 
     }
+
     private fun llamar(y: View?) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
             != PackageManager.PERMISSION_GRANTED
@@ -33,7 +38,9 @@ class MainActivity : AppCompatActivity() {
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(
                     this,
-                    Manifest.permission.CALL_PHONE)) {
+                    Manifest.permission.CALL_PHONE
+                )
+            ) {
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
@@ -52,28 +59,46 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun camera(y: View?){
+    val REQUEST_IMAGE_CAPTURE = 1;
+    private fun camera(y: View?) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED) {
+            != PackageManager.PERMISSION_GRANTED
+        ) {
             // Permission is not granted
             // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.CAMERA)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.CAMERA
+                )
+            ) {
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
                 longToast("Permitido")
-                ActivityCompat.requestPermissions(this,
+                ActivityCompat.requestPermissions(
+                    this,
                     arrayOf(Manifest.permission.CAMERA),
-                    1)
+                    1
+                )
             } else {
                 longToast("Rechazado permanentemente")
 
             }
         } else {
             longToast("Ya permitido")
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(intent,0)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imagenBitmap = data?.extras?.get("data") as Bitmap
+            imagen.setImageBitmap(imagenBitmap)
+        }
+    }
+
 }
